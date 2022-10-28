d70f016cb6f005115eec0a90f352eb961b77dde0
benjamin <b.james.neustadt@gmail.com>
2022-10-28 16:29:31 +0200

    Switch image inside button when theme switches

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,23 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const theme = document.getElementById("theme");
+const theme_img = document.getElementById("theme-img");
 let darkMode = localStorage.getItem("dark-mode");
 
 const enableDarkMode = () => {
   theme.classList.add("dark-mode-theme");
   toggleBtn.classList.remove("dark-mode-toggle");
+  theme_img.classList.remove("night-theme-img");
+  theme_img.classList.add("day-theme-img");
+  toggleBtn.classList.add("dark-mode-toggle-light");
   localStorage.setItem("dark-mode", "enabled");
 };
 
 const disableDarkMode = () => {
   theme.classList.remove("dark-mode-theme");
+  toggleBtn.classList.remove("dark-mode-toggle-light");
+  theme_img.classList.remove("day-theme-img");
   toggleBtn.classList.add("dark-mode-toggle");
+  theme_img.classList.add("night-theme-img");
   localStorage.setItem("dark-mode", "disabled");
 };
 
